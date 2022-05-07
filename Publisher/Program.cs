1446f3116914a17af1d530d14b08a9409f9d54e9
Berat Özmen <beratzmen@gmail.com>
2022-05-07 17:40:57 +0300

    first commit

---
+++ Publisher/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+/// <summary>Kuyruk oluşturma metodu. Parametreleri publisher-subscriber aynı olmalıdır. Eğer bu method eklenmezse, kuyruk yoksa hata verir.</summary>
+/// <param name="queue">Kuyruk ismi.</param>
+/// <param name="durable">False yapılırsa, rabbitmq'da oluşan kuyruklar memory'de tutulur. True yapılırsa fiziksel olarak kaydedilir restart edilse bile kuyruklar kaybolmaz</param>
+/// <param name="exclusive">True yapılırsa sadece buradaki kanal üzerinden bağlanılabilir, farklı kanallar(Instance) üzerinden bağlanılmak istenirse FALSE yapılmalıdır.</param>
+/// <param name="auto delete">True yapılırsa,kuyruğa bağlı olan son subscriber bağlantısını kopartırsa kuyruk silinir.</param>
+channel.QueueDeclare("queue", true, false, false);
+
+string message = "";
+
+for (int i = 1; i <= 50; i++)
+{
+    message = $"Message: {i}";
+
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    /// <summary>Mesajı kuyruğa yayınlayan metot.</summary>
+    /// <param name="exchange">Exchange kullanılmadığı, direk olarak kuyruğa gönderildiği için Empty geçilir.</param>
+    /// <param name="routingKey">Exchange kullanılmazsa Default Exchange olarak kullanılır. Mutlaka kuyruğun ismi verilir.</param>
+    /// <param name="body">Byte olarak mesaj içeriği verilir.</param>
+    channel.BasicPublish(string.Empty, "queue", null, messageBody);
+
+    Thread.Sleep(1500);
+
+    Console.WriteLine($"Mesaj Gönderildi: {message}");
+}
+
+Console.ReadLine();
---
+++ Subscriber/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("queue", true, false, false);
+
+/// <summary>Her bir subscriber'a kaç mesaj iletilecek.</summary>
+/// <param name="prefetchCount">Her bir subscriber'a 1 mesaj gelsin.</param>
+/// <param name="global">True yapılırsa, kaç subscriber varsa tek bir seferde subscriber'ların sayısına bölerek gönderir. False yapılırsa her subscriber için kaçar tane gönderileceği ayarlanır.</param>
+channel.BasicQos(0, 1, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+/// <summary>İlgili queue'da işlem başarılıysa bildirim işlemi yapılır ve kaydı siler.</summary>
+/// <param name="queue">Kuyruk ismi.</param>
+/// <param name="autoAck">True verilirse, kuyruk mesajı subscriber'a gönderdiği zaman kuyruktan mesajı siler. False verilirse, kuyruktan silinmez mesaj doğru işlenmişse kuyruğa haber verilir ve kuyruktan mesaj silinir.</param>
+/// <param name="IBasicConsumer">Consumer.</param>
+channel.BasicConsume("queue", false, consumer);
+
+/// <summary>Rabbitmq subscriber'a mesaj gönderdiği zaman bu metot çalışır.</summary>
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+    Console.WriteLine($"Rabbitmq gelen mesaj: {message}");
+
+    /// <summary>Gelen mesaj başarılı işlendiyse bu metot ile ilgili mesajı sileriz.</summary>
+    /// <param name="deliveryTag">Rabbitmq hangi TAG ile mesajı ilettiyse o mesajı siliyoruz.</param>
+    /// <param name="multiple">True verilirse, o anda memory'de işlenmiş ama rabbitmq gitmemiş mesajlar varsa haber verilir. False verilirse, her bir bilgi ile ilgili mesajın durumunu rabbitmq'ya bildiririz.</param>
+    channel.BasicAck(e.DeliveryTag, false);
+};
+
+Console.ReadLine();
