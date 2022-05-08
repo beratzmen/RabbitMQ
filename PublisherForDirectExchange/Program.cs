5afe9309cbc48ef1d813a385b338222379968a13
Berat Özmen <beratzmen@gmail.com>
2022-05-08 12:30:47 +0300

    Publisher and Subscriber for DirextExchange added, messages are written to txt file.

---
+++ PublisherForDirectExchange/Program.cs
@@ -0,0 +1,52 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("logs-direct", durable: true, type: ExchangeType.Direct);
+
+foreach (var error in Enum.GetNames(typeof(LogNames)))
+{
+    var routeKey = $"route-{error}";
+
+    var queueName = $"direct-queue-{error}";
+
+    channel.QueueDeclare(queueName, true, false, false);
+
+    channel.QueueBind(queueName, "logs-direct", routeKey, null);
+}
+
+string message = "";
+
+for (int i = 1; i <= 50; i++)
+{
+    Thread.Sleep(1500);
+
+    LogNames log = (LogNames)new Random().Next(1, 5);
+
+    message = $"Log-Type: {log}";
+
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    var routeKey = $"route-{log}";
+
+    channel.BasicPublish("logs-direct", routeKey, null, messageBody);
+
+    Console.WriteLine($"Log Gönderildi: {message}");
+}
+
+Console.ReadLine();
+
+enum LogNames
+{
+    Critical = 1,
+    Error = 2,
+    Warning = 3,
+    Information = 4
+}
---
+++ SubscriberForDirectExchange/Program.cs
@@ -0,0 +1,40 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.BasicQos(0, 1, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+var queueName = "direct-queue-Error";
+
+channel.BasicConsume(queueName, false, consumer);
+
+Console.WriteLine("Loglar dinleniyor..");
+
+/// <summary>Rabbitmq subscriber'a mesaj gönderdiği zaman bu metot çalışır.</summary>
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+    Console.WriteLine($"Rabbitmq gelen mesaj: {message}");
+
+    Thread.Sleep(1000);
+
+    File.AppendAllText("logs-Error.txt", message + "\n");
+
+    /// <summary>Gelen mesaj başarılı işlendiyse bu metot ile ilgili mesajı sileriz.</summary>
+    /// <param name="deliveryTag">Rabbitmq hangi TAG ile mesajı ilettiyse o mesajı siliyoruz.</param>
+    /// <param name="multiple">True verilirse, o anda memory'de işlenmiş ama rabbitmq gitmemiş mesajlar varsa haber verilir. False verilirse, her bir bilgi ile ilgili mesajın durumunu rabbitmq'ya bildiririz.</param>
+    channel.BasicAck(e.DeliveryTag, false);
+};
+
+Console.ReadLine();
