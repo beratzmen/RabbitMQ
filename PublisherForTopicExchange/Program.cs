4a4c5f73edb14ef7dc620bded61deabc5f08d094
Berat Özmen <beratzmen@gmail.com>
2022-05-14 10:52:47 +0300

    Publisher and Subscriber for TopicExchange added.

---
+++ PublisherForTopicExchange/Program.cs
@@ -0,0 +1,45 @@
+﻿
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("logs-topic", durable: true, type: ExchangeType.Topic);
+
+string message = "";
+
+for (int i = 1; i <= 50; i++)
+{
+    var random = new Random();
+    var log1 = (LogNames)random.Next(1, 5);
+    var log2 = (LogNames)random.Next(1, 5);
+    var log3 = (LogNames)random.Next(1, 5);
+
+    //Critical.Error.Information şeklinde bir kuyruk ismi oluşturmak için.
+    var routeKey = $"{log1}.{log2}.{log3}";
+
+    message = $"Log-Type: {log1}-{log2}-{log3}";
+
+    var messageBody = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish("logs-topic", routeKey, null, messageBody);
+
+    Console.WriteLine($"Log Gönderildi: {message}");
+
+    Thread.Sleep(1500);
+}
+
+Console.ReadLine();
+
+enum LogNames
+{
+    Critical = 1,
+    Error = 2,
+    Warning = 3,
+    Information = 4
+}
---
+++ SubscriberForTopicExchange/Program.cs
@@ -0,0 +1,42 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.BasicQos(0, 1, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+var queueName = channel.QueueDeclare().QueueName;
+
+//"*.*.Warning" => Sonunda Warning olan kuyruğa bağlanır. * tek bir karaktere karşılık gelir.
+//Information.# => Başı Information olsun, sonunda ne var ne yok benim için önemli değil demek için # yapılır.
+//Ortası Error olan başı ve sonu önemli olmayan route.
+var routeKey = "*.Error.*";
+
+//logs-topic exchange' indeki, başı sonu önemli değil ortasında Error yazan kuyruğa bağlan demek için.
+channel.QueueBind(queueName, "logs-topic", routeKey);
+
+channel.BasicConsume(queueName, false, consumer);
+
+Console.WriteLine("Loglar dinleniyor..");
+
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+    Console.WriteLine($"Rabbitmq gelen mesaj: {message}");
+
+    channel.BasicAck(e.DeliveryTag, false);
+
+    Thread.Sleep(1000);
+};
+
+Console.ReadLine();
