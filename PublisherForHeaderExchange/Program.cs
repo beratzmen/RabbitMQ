e9cfbc1fc2c9b3b7eabf2de7deebd1bcf9d84c09
Berat Özmen <beratzmen@gmail.com>
2022-05-18 18:46:45 +0300

    Publisher and Subscriber for HeaderExchange added.

---
+++ PublisherForHeaderExchange/Program.cs
@@ -0,0 +1,49 @@
+﻿
+using RabbitMQ.Client;
+using Shared;
+using System.Text;
+using System.Text.Json;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+
+var dictionary = new Dictionary<string, object>();
+
+dictionary.Add("format", "pdf");
+dictionary.Add("shape", "a4");
+
+var properties = channel.CreateBasicProperties();
+
+properties.Headers = dictionary;
+//Mesajlar kalıcı hale getirildi. Rabbitmq restart edilse bile mesajlar kaybolmaz.
+properties.Persistent = true;
+
+var product = new Product
+{
+    Id = 1,
+    Name = "Product1",
+    Price = 5,
+    Stock = 55
+};
+
+var productJsonString = JsonSerializer.Serialize(product);
+
+channel.BasicPublish("header-exchange", string.Empty, properties, Encoding.UTF8.GetBytes(productJsonString));
+
+Console.WriteLine("Mesaj gönderilmiştir.");
+
+Console.ReadLine();
+
+enum LogNames
+{
+    Critical = 1,
+    Error = 2,
+    Warning = 3,
+    Information = 4
+}
---
+++ SubscriberForHeaderExchange/Program.cs
@@ -0,0 +1,46 @@
+﻿
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using Shared;
+using System.Text;
+using System.Text.Json;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+using var connection = factory.CreateConnection();
+
+var channel = connection.CreateModel();
+
+channel.BasicQos(0, 1, false);
+
+var consumer = new EventingBasicConsumer(channel);
+
+var queueName = channel.QueueDeclare().QueueName;
+
+var dictionary = new Dictionary<string, object>();
+dictionary.Add("format", "pdf");
+dictionary.Add("shape", "a4");
+//'All' - Tüm değerlerin eşleşmesi gerekir(Publisher). All yerine 'Any' yapılırsa, eşleşen herhangi biri alınır.
+dictionary.Add("x-match", "all"); 
+
+channel.QueueBind(queueName, "header-exchange", string.Empty, dictionary);
+
+channel.BasicConsume(queueName, false, consumer);
+
+Console.WriteLine("Loglar dinleniyor..");
+
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+    var product = JsonSerializer.Deserialize<Product>(message);
+
+    Console.WriteLine($"Rabbitmq gelen mesaj: {product.Id} - {product.Name} - {product.Price} - {product.Stock}");
+
+    channel.BasicAck(e.DeliveryTag, false);
+
+    Thread.Sleep(1000);
+};
+
+Console.ReadLine();
