c95f0eafc2517c8a4e2551abc64fd9d0441a8615
Berat Özmen <beratzmen@gmail.com>
2022-05-21 14:25:01 +0300

    Connection strings and migration added

---
+++ WebExcelCreate/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebExcelCreate.Models
+{
+    public class Context : IdentityDbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+        }
+
+        public DbSet<UserFile> UserFiles { get; set; }
+    }
+}
--- WebExcelCreate/Program.cs
+++ WebExcelCreate/Program.cs
@@ -1,4 +1,21 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using WebExcelCreate.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//////*****************************************************************************************************//////
+/*EFCORE Db ConnectionString*/
+builder.Services.AddDbContext<Context>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+});
+
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options =>
+{
+    options.User.RequireUniqueEmail = true;
+}).AddEntityFrameworkStores<Context>();
+//////*****************************************************************************************************//////
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
@@ -14,10 +31,33 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+//////*****************************************************************************************************//////
+app.UseRouting();
+app.UseAuthentication();
+//////*****************************************************************************************************//////
+
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+//////*****************************************************************************************************//////
+///Program ayaða kalkarken seed kayýt eklendi
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<Context>();
+
+    var userManager = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+    context.Database.Migrate();
+
+    if (!context.Users.Any())
+    {
+        userManager.CreateAsync(new IdentityUser { UserName = "berat", Email = "berat@gmail.com" }, "berat").Wait();
+        userManager.CreateAsync(new IdentityUser { UserName = "berat2", Email = "berat2@gmail.com" }, "berat2").Wait();
+    }
+}
+//////*****************************************************************************************************//////
 
 app.Run();
